051b30a8ebf4bb8e44edfe61eabf982682858092
AnnemariaRe <annemariarepenko@gmail.com>
2023-04-16 21:15:32 +0300

    add: HRS config

---
+++ HRS-service/src/main/java/ru/nexign/hrs/HrsServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.nexign.hrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrsServiceApplication.class, args);
+    }
+
+}
---
+++ HRS-service/src/test/java/ru/nexign/hrs/HrsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nexign.hrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
