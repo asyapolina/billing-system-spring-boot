76ceb39771ed45ecb74ca33f1e10b5b613faa3db
asyapolina <rin.mori23@gmail.com>
2023-04-29 22:32:54 +0300

    add tests fro PhoneNumber

--- CDR-service/src/test/java/ru/nexign/cdr/TariffGeneratorTests.java
+++ CDR-service/src/test/java/ru/nexign/cdr/TariffGeneratorTests.java
@@ -2,7 +2,6 @@ package ru.nexign.cdr;
 
 import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import ru.nexign.cdr.generator.client.TariffGenerator;
 
@@ -13,7 +12,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class TariffGeneratorTests {
     @Test
     @DisplayName("Ensure that TariffGenerators return valid values")
-    void testCanConstructDefault() {
+    void testReturnValidTariffs() {
         ArrayList<String> validValues = Lists.newArrayList("03", "06", "11");
         for (int i = 0; i < 10; ++i) {
             assertTrue(validValues.contains(TariffGenerator.generateRandomTariffId()));
